25eeb2754a8d00bff283296d8f37b98e63d17915
pungamuruvasi@gmail.com <pungamuruvasi@gmail.com>
2024-01-09 18:45:33 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import plotly.graph_objects as go
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def image_1_mouse_move(self, x, y, **event_args):
+    """This method is called when the mouse cursor moves over this component"""
+    pass
+
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
