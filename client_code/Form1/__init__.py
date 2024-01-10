789b7a96b5f50e100c57699e68b28abbc29a160b
pungamuruvasi@gmail.com <pungamuruvasi@gmail.com>
2024-01-10 15:42:45 +0000

    Created 11.png, Screenshot.png and ai IMAGE.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import plotly.graph_objects as go
 
 class Form1(Form1Template):
